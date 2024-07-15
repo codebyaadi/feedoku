5efe1026152657c173d29151fddef9de8adbcd89
Aditya Rajbhar <100792725+codebyaadi@users.noreply.github.com>
2024-07-15 05:22:53 +0000

    chore: moved main.go to cmd/main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
@@ -45,11 +49,32 @@ func main() {
 
 	mux.HandleFunc("/", handler)
 
-	// Start HTTP server.
-	log.Printf("listening on port %s", port)
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		log.Fatal(err)
+	addr := ":" + port
+	server := &http.Server{
+		Addr:    addr,
+		Handler: mux,
 	}
+
+	shutdownCh := make(chan os.Signal, 1)
+	signal.Notify(shutdownCh, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		log.Printf("listening on port %s", port)
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("could not listen on %s: %v\n", port, err)
+		}
+	}()
+
+	<-shutdownCh
+	log.Println("shutting down...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("could not gracefully shutdown the server: %v\n", err)
+	}
+	log.Println("server stopped")
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
