aad90fb92580344d6a8c7cf8100ee8f04a38f9cd
Aditya Rajbhar <100792725+codebyaadi@users.noreply.github.com>
2024-09-15 07:42:59 +0000

    site config added

---
+++ www/src/config/site.ts
@@ -0,0 +1,16 @@
+export type SiteConfig = typeof siteConfig;
+
+const links = {
+  x: 'https://twitter.com/codebyaadi',
+  github: 'https://github.com/codebyaadi/rss-feed-agg',
+  githubAccount: 'https://github.com/codebyaadi',
+};
+
+export const siteConfig = {
+  name: 'Feedify',
+  description:
+    'An open source RSS Feed Aggregator created using Golang and Next.js',
+  url: 'https://',
+  ogImage: 'https://',
+  links,
+};
