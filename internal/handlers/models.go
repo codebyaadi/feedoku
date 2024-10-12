d68141cbecccf1a76e1d86a7d9ba1415eef02c15
Aditya Rajbhar <100792725+codebyaadi@users.noreply.github.com>
2024-10-12 06:29:23 +0000

    chore: removed the unnecssary comments

--- internal/handlers/models.go
+++ internal/handlers/models.go
@@ -13,26 +13,16 @@ type Handler struct {
 }
 
 type User struct {
-	ID        uuid.UUID `json:"id"`
-	Name      string    `json:"name"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-	ApiKey    string    `json:"api_key"`
-	Email     string    `json:"email"`
-	AccessToken string	`json:"access_token,omitempty"`
-	RefreshToken string `json:"refresh_token,omitempty"`
+	ID           uuid.UUID `json:"id"`
+	Name         string    `json:"name"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
+	ApiKey       string    `json:"api_key"`
+	Email        string    `json:"email"`
+	AccessToken  string    `json:"access_token,omitempty"`
+	RefreshToken string    `json:"refresh_token,omitempty"`
 }
 
-// convertDatabaseUserToAPIUser converts a database user model to an API user model.
-//
-// Parameters:
-//   - dbUser: The user model from the database, typically containing fields like ID, Name, CreatedAt, and UpdatedAt.
-//
-// Returns:
-//   - A User struct containing the same ID, Name, CreatedAt, and UpdatedAt fields, formatted for API responses.
-//
-// This function maps the fields from the database-specific User struct to the API-specific
-// User struct, ensuring the data is properly formatted for JSON serialization and API responses.
 func convertDatabaseUserToAPIUser(dbUser database.User, accessToken, refreshToken string) User {
 	user := User{
 		ID:        dbUser.ID,
@@ -62,16 +52,6 @@ type Feed struct {
 	UserID    uuid.UUID `json:"user_id"`
 }
 
-// convertDatabaseFeedToAPIFeed converts a database feed model to an API feed model.
-//
-// Parameters:
-//   - dbFeed: The feed model from the database, typically containing fields like ID, Name, CreatedAt, UpdatedAt, Url, and UserID.
-//
-// Returns:
-//   - A Feed struct containing the same ID, Name, CreatedAt, UpdatedAt, Url, and UserID fields, formatted for API responses.
-//
-// This function maps the fields from the database-specific Feed struct to the API-specific
-// Feed struct, ensuring the data is properly formatted for JSON serialization and API responses.
 func convertDatabaseFeedToAPIFeed(dbFeed database.Feed) Feed {
 	return Feed{
 		ID:        dbFeed.ID,
@@ -83,17 +63,6 @@ func convertDatabaseFeedToAPIFeed(dbFeed database.Feed) Feed {
 	}
 }
 
-// convertDatabaseFeedsToAPIFeeds converts a slice of database feed models to a slice of API feed models.
-//
-// Parameters:
-//   - dbFeeds: A slice of database feed models, each containing fields like ID, Name, CreatedAt, UpdatedAt, Url, and UserID.
-//
-// Returns:
-//   - A slice of Feed structs, each containing the same ID, Name, CreatedAt, UpdatedAt, Url, and UserID fields,
-//     formatted for API responses.
-//
-// This function iterates over each database feed model, converts it to an API feed model using
-// convertDatabaseFeedToAPIFeed, and returns a slice of these API feed models.
 func convertDatabaseFeedsToAPIFeeds(dbFeeds []database.Feed) []Feed {
 	feeds := make([]Feed, len(dbFeeds))
 	for i, dbFeed := range dbFeeds {
